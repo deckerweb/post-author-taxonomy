89bcfbbf09139d3d760b957ca0122e5fcaa1f975
David Decker <deckerweb.mobil@gmail.com>
2025-03-31 17:10:04 +0200

    Bring plugin back to life; prepare for next version & release

---
+++ README.txt
@@ -0,0 +1,141 @@
+# Post Author Taxonomy
+
+**Simple & lightweight:** Registers a simple post authors taxonomy for the regular Posts post type. The taxonomy is not hierarchical, so it's similar to tags but for authors instead. Shortcodes for a list of authors and an Author Box are included.
+
+![Post Author Taxonomy plugin banner](https://repository-images.githubusercontent.com/36257371/b329de67-e904-47d5-998d-0031c49f59a1)
+
+### Tested Compatibility
+- **WordPress**: 6.7.2 / 6.8 Beta
+- **PHP**: 8.0 – 8.3
+- Requires at least: WP 6.7 / PHP 7.4
+
+---
+
+[Support Project](#support-the-project) | [Installation](#installation) | [Description](#description) | [Features](#features) | [Usage](#usage---examples) | [Shortcode Parameters](#shortcode-parameters) | [Widgets](#widget-usage) | [Templates](#template-usage-developers) | [Filters](#plugin-filters-developers) | [Changelog](#changelog--version-history) | [Plugin Scope / Disclaimer](#plugin-scope--disclaimer)
+
+---
+
+## Support the Project
+
+If you find this project helpful, consider showing your support by buying me a coffee! Your contribution helps me keep developing and improving this plugin.
+
+Enjoying the plugin? Feel free to treat me to a cup of coffee ☕🙂 through the following options:
+
+- [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/W7W81BNTZE)
+- [Buy me a coffee](https://buymeacoffee.com/daveshine)
+- [PayPal donation](https://paypal.me/deckerweb)
+- [Join my **newsletter** for DECKERWEB WordPress Plugins](https://eepurl.com/gbAUUn)
+
+---
+
+## Installation
+
+#### **Quick Install – as Plugin**
+1. **Download ZIP:** [**post-author-taxonomy.zip**](https://github.com/deckerweb/post-author-taxonomy/releases/latest/download/post-author-taxonomy.zip)
+2. Upload via WordPress Plugins > Add New > Upload Plugin
+3. Once activated, you can use the shortcode – [see Usage below](#usage---examples)
+ 
+#### **Alternative: Use as Code Snippet**
+1. Below, download the appropriate snippet version
+2. activate or deactivate in your snippets plugin
+
+[**Download .json**](https://github.com/deckerweb/post-author-taxonomy/releases/latest/download/ddw-post-author-taxonomy.code-snippets.json) version for: _Code Snippets_ (free & Pro), _Advanced Scripts_ (Premium), _Scripts Organizer_ (Premium)
+--> just use their elegant script import features
+--> in _Scripts Organizer_ use the "Code Snippets Import"
+
+For all other snippet manager plugins just use our plugin's main .php file [`post-author-taxonomy.php`](https://github.com/deckerweb/post-author-taxonomy/blob/master/post-author-taxonomy.php) and use its content as snippet (bevor saving your snippet: please check for your plugin if the opening php tag needs to be removed or not!).
+
+--> Please decide for one of both alternatives!
+
+---
+
+## Description
+
+Very useful to add and display a list of authors to a blog post. If you don't want to use the built-in author, then this plugin/snippet is for you. By default WordPress can only handle one author per post (or post type). Handling multiple authors via a taxonomy is the simplest and most lightweight way to do that. You can then work with that data in your content, templates and your favorite builder.
+
+*Backstory:* I needed something like that for a client project to display a simple list of post authors. The client didn't wanted to add each author as WordPress user therefore the plugin "Co Authors Plus" was not a possible option. Also, the taxonomy solution is simpler, more user friendly and more flexible.
+
+---
+
+## Features
+
+* By default an `Authors` Taxonomy added for regular WordPress Posts (post type: `post`)
+* Shortcode for adding a list of authors, for example in a single post (template) – comma separated list of authors
+* Shortcode for adding an author box, for example in a single post (or template) – one author per box
+* Can be used with Classic Editor and the Block Editor (Gutenberg)
+* Can be used perfectly fine with most Page Builders (Bricks, Breakdance, Oxygen, Elementor etc.) – just use the taxonomy data in your favorite builder's dynamic data feature, you won't need the Shortcodes I guess :-)
+* Developer friendly: customize or extend via filters, styles and styling-friendly CSS classes
+* Fully internationalized and translateable! -- German translations already packaged!
+* Developed with security in mind: proper WordPress coding standards and security functions - escape all the things! :)
+
+---
+
+## Usage - Examples
+
+(content upcoming)
+
+---
+
+## Shortcode Parameters
+
+(content upcoming)
+
+---
+
+## Widget Usage
+
+NO LONGER recommended! Widgets are outdated, I do not recommend them (and _didn't_ use them myself for lots of years already!).
+
+Shortcode could be used with "Text" widget -- if you have Shortcodes for Widgets activated. Possible via this filter:
+```
+add_filter( 'widget_text', 'do_shortcode' );
+```
+If using extended/ advanced text widget plugins, the Shortcode usage then is already enabled automatically... :-)
+
+---
+
+## Template Usage (Developers)
+
+Use WordPress' global `do_shortcode()` function as a template function, like so:
+```
+<?php do_shortcode( '[pat-authors]' ); ?>
+```
+--> parameters apply like for regular Shortcode usage (see above)!
+
+---
+
+## Plugin Filters (Developers)
+
+* `pat/filter/taxonomy/params` --> filter all Taxonomy parameters
+* `pat/filter/shortcode/authors_list_defaults` --> filter authors list Shortcode defaults
+* `pat/filter/shortcode/authors-list` --> filter authors list Shortcode output
+* `pat/filter/shortcode/author_box_defaults` --> filter author box Shortcode defaults
+* `pat/filter/shortcode/author-box` --> filter author box Shortcode output
+
+---
+
+## Changelog – Version History
+
+### 🎉 v1.2.0 – 2025-03-??
+* Updated plugin after 8 years, yeah! – Brought back to its basic beauty. (How it should be!)
+* New: Transformed code into class-based approach (more future-proof)
+* New: Shortcode `pat-author-box` for displaying a simple author box (content = term description)
+* New: Alternate install: Use "plugin" as Code Snippet version – now officially promoted here in Readme and with downloadable `.json` file
+* Improved: Shortcode `pat-authors` enhanced
+* Change: Simplified additional translation loading (needed, for proper usage/display, as this plugin comes not from .org repository)
+* Plugin: Add meta links on WP Plugins page
+* Updated `.pot` file, plus packaged German translations
+
+
+### 🎉 v1.1.0 – 2018-09-18
+* Unreleased _private_ version
+
+
+### 🎉 v1.0.0 – 2017-12-15
+* Initial _public_ release on GitHub
+
+---
+
+Icon used in promo graphics: © Remix Icon
+
+Copyright: © 2017-2025, David Decker – DECKERWEB.de
--- post-author-taxonomy.php
+++ post-author-taxonomy.php
@@ -1,136 +1,403 @@
 <?php # -*- coding: utf-8 -*-
-/**
- * Main plugin file.
- * @package           Post Author Taxonomy
- * @author            David Decker
- * @copyright         Copyright (c) 2017, David Decker - DECKERWEB
- * @license           GPL-2.0+
- * @link              https://deckerweb.de/twitter
- *
- * @wordpress-plugin
- * Plugin Name:       Post Author Taxonomy
- * Plugin URI:        https://github.com/deckerweb/post-author-taxonomy
- * Description:       Registers a simple post authors taxonomy for the regular Posts post type. The taxonomy is not hierarchical, so it's similar to taks but for authors instead.
- * Version:           2017.12.15
- * Author:            David Decker - DECKERWEB
- * Author URI:        https://deckerweb.de/
- * License:           GPL-2.0+
- * License URI:       http://www.opensource.org/licenses/gpl-license.php
- * Text Domain:       post-author-taxonomy
- * Domain Path:       /languages/
- * GitHub Plugin URI: https://github.com/deckerweb/post-author-taxonomy
- * GitHub Branch:     master
- *
- * Copyright (c) 2017 David Decker - DECKERWEB
- */
+/*
+Plugin Name:       Post Author Taxonomy
+Plugin URI:        https://github.com/deckerweb/post-author-taxonomy
+Description:       Registers a simple post authors taxonomy for the regular Posts post type. The taxonomy is not hierarchical, so it's similar to tags but for authors instead. Shortcodes for a list of authors and an Author Box are included. For developers there a lot of filters to extend it for more post types, plus optionally customize stuff.
+Project:           Code Snippet: DDW Post Author Taxonomy
+Version:           1.2.0
+Author:            David Decker - DECKERWEB
+Author URI:        https://deckerweb.de/
+License:           GPL-2.0-or-later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:       post-author-taxonomy
+Domain Path:       /languages/
+Requires WP:       6.7
+Requires PHP:      7.4
+GitHub Plugin URI: https://github.com/deckerweb/post-author-taxonomy
+GitHub Branch:     master
+Copyright:         © 2017-2025, David Decker - DECKERWEB
+*/
 
 /**
  * Exit if called directly.
  */
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
+if ( ! defined( 'ABSPATH' ) ) exit( 'Sorry, you are not allowed to access this file directly.' );
 
-add_action( 'init', 'ddw_pat_load_translations', 1 );
+
+if ( ! class_exists( 'DDW_Post_Author_Taxonomy' ) ) :
+
+class DDW_Post_Author_Taxonomy {
+
+	/** Class constants & variables */
+	private const VERSION = '1.2.0';
+	private const DEFAULT_MENU_POSITION	= 999;  // default: 999
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action(    'init',           array( $this, 'register_taxonomy' ), 100 );
+		add_shortcode( 'pat-authors',    array( $this, 'shortcode_authors_list' ) );
+		add_shortcode( 'pat-author-box', array( $this, 'shortcode_author_box' ) );
+		//add_filter(    'debug_information', array( $this, 'site_health_debug_info' ), 9 );
+	}
+	
+	/**
+	 * Register custom taxomoy: Post Authors
+	 *
+	 * @since  1.0.0 (2017.12.15)
+	 * @since  1.1.0 Added more filters.
+	 *
+	 * @return object Taxonomy declaration
+	 */
+	public function register_taxonomy() {
+	
+		/** Load our plugin translation, as there are no language packs from wordpress.org */
+		load_plugin_textdomain( 'post-author-taxonomy', FALSE, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	
+		/** Define tax labels/ wording */
+		$labels = array(
+			'name'                       => _x( 'Authors', 'Taxonomy General Name', 'post-author-taxonomy' ),
+			'singular_name'              => _x( 'Author', 'Taxonomy Singular Name', 'post-author-taxonomy' ),
+			'all_items'                  => __( 'All Authors', 'post-author-taxonomy' ),
+			'parent_item'                => __( 'Parent Author', 'post-author-taxonomy' ),
+			'parent_item_colon'          => __( 'Parent Author:', 'post-author-taxonomy' ),
+			'new_item_name'              => __( 'New Author Name', 'post-author-taxonomy' ),
+			'add_new_item'               => __( 'Add New Author', 'post-author-taxonomy' ),
+			'edit_item'                  => __( 'Edit Author', 'post-author-taxonomy' ),
+			'update_item'                => __( 'Update Author', 'post-author-taxonomy' ),
+			'view_item'                  => __( 'View Author', 'post-author-taxonomy' ),
+			'separate_items_with_commas' => __( 'Separate Authors with commas', 'post-author-taxonomy' ),
+			'add_or_remove_items'        => __( 'Add or remove Authors', 'post-author-taxonomy' ),
+			'choose_from_most_used'      => __( 'Choose from the most used', 'post-author-taxonomy' ),
+			'popular_items'              => __( 'Popular Authors', 'post-author-taxonomy' ),
+			'search_items'               => __( 'Search Auhtors', 'post-author-taxonomy' ),
+			'not_found'                  => __( 'Not Found', 'post-author-taxonomy' ),
+			'no_terms'                   => __( 'No Authors', 'post-author-taxonomy' ),
+			'items_list'                 => __( 'Authors list', 'post-author-taxonomy' ),
+			'items_list_navigation'      => __( 'Authors list navigation', 'post-author-taxonomy' ),
+		);
+	
+		/** Declare rewrite rules */
+		$rewrite = array(
+			/* translators: slug part for Post Author Taxonomy in the URLs */
+			'slug'         => sanitize_key(
+				_x(
+					'post-author',
+					'Translators: slug part for Post Author Taxonomy in the URLs',
+					'post-author-taxonomy'
+				)
+			),
+			'with_front'   => TRUE,
+			'hierarchical' => FALSE,
+		);
+	
+		/** Declare tax params */
+		$args = array(
+			'labels'            => $labels,
+			'hierarchical'      => FALSE,
+			'public'            => TRUE,
+			'show_ui'           => TRUE,
+			'show_admin_column' => TRUE,
+			'show_in_nav_menus' => TRUE,
+			'show_tagcloud'     => TRUE,
+			'show_in_rest'      => TRUE,
+			'rewrite'           => $rewrite,
+			'meta_box_cb'       => 'post_categories_meta_box',	// only for Classic, not for Gutenberg Editor
+			'description'       => __( 'A simple post authors taxonomy for the regular Posts post type.', 'post-author-taxonomy' ),
+		);
+	
+		/** Finally, register the taxonomy */
+		register_taxonomy(
+			'pat-author',
+			array( 'post' ),
+			apply_filters(
+				'pat/filter/taxonomy/params',
+				$args
+			)
+		);		
+	}
+	
+	/**
+	 * Produces the tag links list.
+	 *
+	 * Supported shortcode attributes are:
+	 *   after (output after link, default is empty string),
+	 *   before (output before link, default is 'Tagged With: '),
+	 *   sep (separator string between tags, default is ', ').
+	 *
+	 * Output passes through `genesis_post_tags_shortcode` filter before returning.
+	 *
+	 * @since  1.1.0
+	 *
+	 * @param  array|string $atts Shortcode attributes. Empty string if no attributes.
+	 * @return string Return empty string if the `post_tag` taxonomy is not associated with the current post type
+	 *                or if the post has no authors. Otherwise, output for `pat-authors` shortcode.
+	 */
+	public function shortcode_authors_list( $atts ) {
+	
+		/** Bail early, if post type has no support for this taxonomy */
+		if ( ! is_object_in_taxonomy( get_post_type(), 'pat-author' ) ) {
+			return '';
+		}
+	
+		/** Setup the Shortcode defaults */
+		$defaults = apply_filters(
+			'pat/filter/shortcode/authors_list_defaults',
+			array(
+				'before'  => __( 'Authors:', 'post-author-taxonomy' ),
+				'after'   => '',
+				'sep'     => ', ',
+				'class'   => '',
+				'wrapper' => 'span',
+			)
+		);
+	
+		/** Setup the Shortcode attributes */
+		$atts = shortcode_atts(
+			$defaults,
+			$atts,
+			'pat-authors'
+		);
+	
+		$label_before = ! empty( $atts[ 'before' ] ) ? $atts[ 'before' ] . ' ' : '';
+		
+		/** Get the terms list (authors) with use of our arguments */
+		$authors = get_the_term_list(
+			get_the_ID(),
+			'pat-author',
+			$label_before,
+			trim( $atts[ 'sep' ] ) . ' ',
+			$atts[ 'after' ]
+		);
+	
+		/** Bail if error with terms list */
+		if ( is_wp_error( $authors ) ) {
+				return '';
+		}
+	
+		/** Bail if no authors (empty) */
+		if ( empty( $authors ) ) {
+				return '';
+		}
+	
+		/** Prepare output */
+		$output = sprintf(
+			'<%1$s class="pat-authors%2$s">%3$s%4$s</%1$s>',
+			sanitize_key( $atts[ 'wrapper' ] ),
+			! empty( $atts[ 'class' ] ) ? ' ' . sanitize_html_class( $atts[ 'class' ] ) : '',
+			$authors,
+			! empty( $atts[ 'after' ] ) ? ' ' . $atts[ 'after' ] : ''
+		);
+	
+		/** Return output, filterable */
+		return apply_filters(
+			'pat/filter/shortcode/authors-list',
+			$output,
+			$atts
+		);
+	}
+	
+	/**
+	 * Shortcode for an "Author Box"
+	 *
+	 * @since 1.2.0
+	 */
+	public function shortcode_author_box( $atts ) {
+		
+		/** Bail early, if post type has no support for this taxonomy */
+		if ( ! is_object_in_taxonomy( get_post_type(), 'pat-author' ) ) {
+			return '';
+		}
+		
+		/** Setup the Shortcode defaults */
+		$defaults = apply_filters(
+			'pat/filter/shortcode/author_box_defaults',
+			array(
+				'title'       => 'yes',
+				'headline'    => '',
+				'title_tag'   => 'h4',
+				'id'          => '',
+				'slug'        => '',
+				'name'        => '',
+				'content_tag' => 'p',
+				'class'       => '',
+				'wrapper'     => 'div',
+			)
+		);
+		
+		/** Setup the Shortcode attributes */
+		$atts = shortcode_atts(
+			$defaults,
+			$atts,
+			'pat-author-box'
+		);
+		
+		/** Default: get term ID from Shortcode */
+		$term_id = $atts[ 'id' ];
+		
+		/** Alternative I: get Term ID by given slug */
+		$term_by_slug = get_term_by( 'slug', sanitize_key( $atts[ 'slug' ] ), 'pat-author' );
+		$term_id      = empty( $term_id ) ? $term_by_slug->term_id : $term_id;
+		
+		/** Alternative II: get Term ID by given name */
+		$term_by_name = get_term_by( 'name', esc_html( $atts[ 'name' ] ), 'pat-author' );
+		$term_id      = empty( $term_id ) ? $term_by_name->term_id : $term_id;
+		
+		$term_description = wp_strip_all_tags( term_description( absint( $term_id ), 'pat-author' ) );
+		$term_description = sprintf(
+			'<%1$s class="pat-author-box__content" itemprop="description">%2$s</%1$s>',
+			sanitize_key( $atts[ 'content_tag' ] ),
+			$term_description
+		);
+		
+		/** Get author name by ID */
+		$author_by_id = get_term_by( 'id', absint( $term_id ), 'pat-author' );
+		
+		/** Prepre Headline & Author Name */
+		$headline    = ( ! empty( $atts[ 'headline' ] ) ) ? esc_html( $atts[ 'headline' ] ) : '';
+		$author_name = ( 'yes' === sanitize_key( $atts[ 'title' ] ) ) ? $author_by_id->name : '';	
+		
+		/** Title logic: use headline, if empty use author name, if also empty, display nothing */
+		$title = ! empty( $headline ) ? $headline : $author_name;
+		
+		$title_display = sprintf(
+			'<%1$s class="pat-author-box__title">%2$s</%1$s>',
+			sanitize_key( $atts[ 'title_tag' ] ),
+			$title
+		);
+		
+		$output = sprintf(
+			'<%1$s class="pat-author-box%2$s">%3$s%4$s</%1$s>',
+			sanitize_key( $atts[ 'wrapper' ] ),
+			! empty( $atts[ 'class' ] ) ? ' ' . sanitize_html_class( $atts[ 'class' ] ) : '',
+			! empty( $title ) ? $title_display : '',	// only empty if no default, and no headline set
+			$term_description
+		);
+		
+		/** Return output, filterable */
+		return apply_filters(
+			'pat/filter/shortcode/author-box',
+			$output,
+			$atts
+		);
+	}
+	
+}  // end of class
+
+/** Start instance of Class */
+new DDW_Post_Author_Taxonomy();
+	
+endif;
+
+
+if ( ! function_exists( 'ddw_pat_plugin_action_link' ) ) :
+	
+add_action( 'admin_init', 'ddw_pat_plugin_action_link', 100 );
 /**
- * Load the text domain for translation of the plugin.
+ * Finally setup the plugin for the main tasks.
  *
- * @since 2017.12.15
- *
- * @uses  load_textdomain()	       To load translations first from WP_LANG_DIR
- *                                 sub folder.
- * @uses  load_plugin_textdomain() To additionally load default translations
- *                                 from plugin folder (default).
+ * @since 1.1.0
  */
-function ddw_pat_load_translations() {
+function ddw_pat_plugin_action_link() {
 
-	/** Set unique textdomain string */
-	$pat_textdomain = 'post-author-taxonomy';
+	/** Add links to Settings and Menu pages to Plugins page */
+	if ( ( is_admin() || is_network_admin() ) ) {
 
-	/** The 'plugin_locale' filter is also used by default in load_plugin_textdomain() */
-	$locale = apply_filters( 'plugin_locale', get_locale(), $pat_textdomain );
+		add_filter(
+			'plugin_action_links_' . plugin_basename( __FILE__ ),
+			'ddw_pat_custom_taxonomy_links'
+		);
 
-	/** Set filter for WordPress languages directory */
-	$siu_wp_lang_dir = apply_filters(
-		'pat_filter_wp_lang_dir',
-		trailingslashit( WP_LANG_DIR ) . 'plugins/' . $pat_textdomain . '-' . $locale . '.mo'
+		add_filter(
+			'network_admin_plugin_action_links_' . plugin_basename( __FILE__ ),
+			'ddw_pat_custom_taxonomy_links'
+		);
+
+	}  // end if
+
+}  // end function
+
+endif;
+
+
+if ( ! function_exists( 'ddw_pat_custom_taxonomy_links' ) ) :
+	
+/**
+ * Add "Custom Taxonomy" link to Plugins page.
+ *
+ * @since  1.1.0
+ *
+ * @param  array $pat_links (Default) Array of plugin action links.
+ * @return strings $pat_links Settings & Menu Admin links.
+ */
+function ddw_pat_custom_taxonomy_links( $pat_links ) {
+
+	/** Taxonomy Page link */
+	$pat_tax_link = sprintf(
+		'<a class="dashicons-before dashicons-admin-users" href="%s" title="%s">%s</a>',
+		esc_url( admin_url( 'edit-tags.php?taxonomy=pat-author' ) ),
+		/* translators: Title attribute for Post Author Taxonomy tax link */
+		esc_html__( 'Post Authors', 'post-author-taxonomy' ),
+		esc_attr_x( 'Post Authors', 'For Post Authors Taxonomy Plugin', 'post-author-taxonomy' )
 	);
 
-	/** Translations: First, look in WordPress' "languages" folder = custom & update-secure! */
-	load_textdomain(
-		$pat_textdomain,
-		$pat_wp_lang_dir
-	);
+	/** Set the order of the links */
+	if ( ! empty( $pat_tax_link ) ) {
+		array_unshift( $pat_links, $pat_tax_link );
+	}
 
-	/** Translations: Secondly, look in plugin's "languages" folder = default */
-	load_plugin_textdomain(
-		$pat_textdomain,
-		FALSE,
-		trailingslashit( dirname( plugin_basename( __FILE__ ) ) ) . 'languages'
+	/** Display plugin settings links */
+	return apply_filters(
+		'pat/filter/plugins_page/tax_link',
+		$pat_links
 	);
 
 }  // end function
 
+endif;
 
-add_action( 'init', 'ddw_pat_register_author_tax', 1 );
+
+if ( ! function_exists( 'ddw_pat_pluginrow_meta' ) ) :
+	
+add_filter( 'plugin_row_meta', 'ddw_pat_pluginrow_meta', 10, 2 );
 /**
- * Register custom taxomoy: Post Authors
+ * Add plugin related links to plugin page.
  *
- * @since  2017.12.15
- *
- * @return object Taxonomy declaration
+ * @param array  $ddwp_meta (Default) Array of plugin meta links.
+ * @param string $ddwp_file File location of plugin.
+ * @return array $ddwp_meta (Modified) Array of plugin links/ meta.
  */
-function ddw_pat_register_author_tax() {
-
-	/** Define tax labels/ wording */
-	$labels = array(
-		'name'                       => _x( 'Authors', 'Taxonomy General Name', 'post-author-taxonomy' ),
-		'singular_name'              => _x( 'Author', 'Taxonomy Singular Name', 'post-author-taxonomy' ),
-		'menu_name'                  => __( 'Authors', 'post-author-taxonomy' ),
-		'all_items'                  => __( 'All Authors', 'post-author-taxonomy' ),
-		'parent_item'                => __( 'Parent Author', 'post-author-taxonomy' ),
-		'parent_item_colon'          => __( 'Parent Author:', 'post-author-taxonomy' ),
-		'new_item_name'              => __( 'New Author Name', 'post-author-taxonomy' ),
-		'add_new_item'               => __( 'Add New Author', 'post-author-taxonomy' ),
-		'edit_item'                  => __( 'Edit Author', 'post-author-taxonomy' ),
-		'update_item'                => __( 'Update Author', 'post-author-taxonomy' ),
-		'view_item'                  => __( 'View Author', 'post-author-taxonomy' ),
-		'separate_items_with_commas' => __( 'Separate Authors with commas', 'post-author-taxonomy' ),
-		'add_or_remove_items'        => __( 'Add or remove Authors', 'post-author-taxonomy' ),
-		'choose_from_most_used'      => __( 'Choose from the most used', 'post-author-taxonomy' ),
-		'popular_items'              => __( 'Popular Authors', 'post-author-taxonomy' ),
-		'search_items'               => __( 'Search Auhtors', 'post-author-taxonomy' ),
-		'not_found'                  => __( 'Not Found', 'post-author-taxonomy' ),
-		'no_terms'                   => __( 'No Authors', 'post-author-taxonomy' ),
-		'items_list'                 => __( 'Authors list', 'post-author-taxonomy' ),
-		'items_list_navigation'      => __( 'Authors list navigation', 'post-author-taxonomy' ),
+function ddw_pat_pluginrow_meta( $ddwp_meta, $ddwp_file ) {
+ 
+	if ( ! current_user_can( 'install_plugins' ) ) return $ddwp_meta;
+	
+	/** Get current user */
+	$user = wp_get_current_user();
+	
+	/** Build Newsletter URL */
+	$url_nl = sprintf(
+	'https://deckerweb.us2.list-manage.com/subscribe?u=e09bef034abf80704e5ff9809&amp;id=380976af88&amp;MERGE0=%1$s&amp;MERGE1=%2$s',
+	esc_attr( $user->user_email ),
+	esc_attr( $user->user_firstname )
 	);
-
-    /** Declare rewrite rules */
-	$rewrite = array(
-		/* translators: slug part for Post Author Taxonomy in the URLs */
-		'slug'                       => _x( 'post-author', 'Translators: slug part for Post Author Taxonomy in the URLs', 'post-author-taxonomy' ),
-		'with_front'                 => true,
-		'hierarchical'               => false,
+	
+	/** List additional links only for this plugin */
+	if ( $ddwp_file === trailingslashit( dirname( plugin_basename( __FILE__ ) ) ) . basename( __FILE__ ) ) {
+	$ddwp_meta[] = sprintf(
+		'<a class="button button-inline" href="https://ko-fi.com/deckerweb" target="_blank" rel="nofollow noopener noreferrer" title="%1$s">❤ <b>%1$s</b></a>',
+		esc_html_x( 'Donate', 'Plugins page listing', 'post-author-taxonomy' )
 	);
-
-    /** Declare tax params */
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-		'rewrite'                    => $rewrite,
+	
+	$ddwp_meta[] = sprintf(
+		'<a class="button-primary" href="%1$s" target="_blank" rel="nofollow noopener noreferrer" title="%2$s">⚡ <b>%2$s</b></a>',
+		$url_nl,
+		esc_html_x( 'Join our Newsletter', 'Plugins page listing', 'post-author-taxonomy' )
 	);
-
-	/** Finally, register the taxonomy */
-	register_taxonomy(
-		'pat-author',
-		array( 'post' ),
-		apply_filters( 'pat-author-tax-params', $args )
-	);
+	}  // end if
+	
+	return apply_filters( 'pat/filter/plugins_page/meta_links', $ddwp_meta );
 
 }  // end function
+
+endif;
